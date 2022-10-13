e3e5248b786322dc9b7960026095915b7dcb7112
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-13 12:54:26 -0500

    completed first example

--- documentObjectModelExamples.js
+++ documentObjectModelExamples.js
@@ -5,4 +5,10 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const numberOne = document.createElement('p');
+numberOne.classList.add('numberOne');
+numberOne.textContent = "Hey, I'm red!";
+numberOne.style.color = 'red';
+
 container.appendChild(content);
+container.appendChild(numberOne);
