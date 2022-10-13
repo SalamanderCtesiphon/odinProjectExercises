4bb9046d5d7df985afdb9fa80364e51d5e1c2520
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-13 13:16:16 -0500

    completed the example

--- documentObjectModelExamples.js
+++ documentObjectModelExamples.js
@@ -15,7 +15,23 @@ numberTwo.classList.add('numberTwo');
 numberTwo.textContent = "I'm a blue h3!";
 numberTwo.style.color = 'blue';
 
+const numberThree = document.createElement('div');
+numberThree.classList.add('numberThree');
+numberThree.style.border = 'black';
+numberThree.style.background = 'pink';
+
+const numberThreeSubOne = document.createElement('h1');
+numberThreeSubOne.classList.add('numberThreeSubOne');
+numberThreeSubOne.textContent = "I'm a div";
+
+const numberThreeSubTwo = document.createElement('p');
+numberThreeSubTwo.classList.add('numberThreeSubTwo');
+numberThreeSubTwo.textContent = "ME TOO!";
+
+numberThree.appendChild(numberThreeSubOne);
+numberThree.appendChild(numberThreeSubTwo);
 
 container.appendChild(content);
 container.appendChild(numberOne);
 container.appendChild(numberTwo);
+container.appendChild(numberThree);
