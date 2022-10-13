1814a649b0da7a2e342b2de4010e87798c4410e2
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-13 13:02:38 -0500

    completed second exercise

--- documentObjectModelExamples.js
+++ documentObjectModelExamples.js
@@ -10,5 +10,12 @@ numberOne.classList.add('numberOne');
 numberOne.textContent = "Hey, I'm red!";
 numberOne.style.color = 'red';
 
+const numberTwo = document.createElement('h3');
+numberTwo.classList.add('numberTwo');
+numberTwo.textContent = "I'm a blue h3!";
+numberTwo.style.color = 'blue';
+
+
 container.appendChild(content);
 container.appendChild(numberOne);
+container.appendChild(numberTwo);
