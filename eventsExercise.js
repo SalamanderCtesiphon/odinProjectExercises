ad97e9a6674b72bbc72b5b54afb78e6b59063492
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-13 13:42:46 -0500

    continued to work through the examples

--- eventsExercise.js
+++ eventsExercise.js
@@ -1,4 +1,12 @@
-const btn = document.querySelector('#btn');
+/* const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
   alert("Hello World");
-});
+}); */
+
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  } 
+
+  btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
