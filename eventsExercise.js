57449163dfe7c07e47a77e757665cb50f8ac1b8a
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-14 13:56:12 -0500

    everyday

--- eventsExercise.js
+++ eventsExercise.js
@@ -10,3 +10,4 @@ function alertFunction() {
   btn.addEventListener('click', function (e) {
     e.target.style.background = 'blue';
   });
+  //i just wanted to add a comment so that i could guarantee a commit today
